4502f5b7b14670837ccbb2ad9311c42e82f301de
Emmanuel <Eezirim23@gmail.com>
2024-07-30 20:21:21 +0000

    Fully working kayak parser

--- lib/trip_advisor.ml
+++ lib/trip_advisor.ml
@@ -41,13 +41,13 @@ let print_optimal_route ~(origin : Location.t) ~(location_list : Location.t list
 
 
 let run () = 
-  let%bind file = Plane.call_api () in
+  (* let%bind file = Plane.call_api () in
   let script_content = Plane.parse_kayak_for_ids file in
 
   let price = Int.of_string (String.strip script_content ~drop:(fun char -> 
     Char.equal char '"' || Char.equal char '$')) in
 
-  print_int price;
+  print_int price; *)
 
 
   let%bind string_origin_address = Async_interactive.ask_dispatch_gen ~f:(fun input -> Ok input) "Enter origin location" in
@@ -76,7 +76,6 @@ let run () =
     let clusters = Cluster.k_means_clustering ~k:num_days ~points:location_places_list in 
     let%bind () = Deferred.List.iteri ~how:`Sequential clusters ~f:(fun idx cluster ->
       print_optimal_route ~origin:location_origin_address ~location_list:cluster ~day:(idx + 1) ~distance_data
-    ) in
     ) in
     return ()
 ;;
