10067e68d2cf67627a96bb8acaac5d9b6035e254
Andrew <agrantlv@gmail.com>
2024-08-09 16:05:21 +0000

    recommendation

--- lib/recommend.ml
+++ lib/recommend.ml
@@ -1,10 +1,10 @@
-(* open! Core
+open! Core
 open Async
 open! Cohttp
 open Cohttp_async
 
 let create_recommendation_header key = 
-  Cohttp.Header.of_list [("X-Goog-Api-Key", key) ; ("Content-Type", "application/json") ; ("X-Goog-FieldMask" "places.displayName")]
+  Cohttp.Header.of_list [("X-Goog-Api-Key", key) ; ("Content-Type", "application/json") ; ("X-Goog-FieldMask", "places.displayName")]
 ;;
 
 let api = Lazy_deferred.create (fun () -> Reader.file_contents "/home/ubuntu/api" );;
@@ -17,15 +17,28 @@ let call_api ~configured_address =
   return (Jsonaf.of_string body)
 ;;
 
-let call_api ~address ~key =
-  Client.post ~headers:(create_recommendation_header key) (Uri.of_string (address)) >>= fun (_resp, body) ->
+
+
+let create_body ~recommendation_type ~(location : Location.t)= 
+  Cohttp_async.Body.of_string([%string {|{"includedTypes": ["%{recommendation_type}"],
+    "maxResultCount": 10,
+    "locationRestriction": {
+      "circle": {
+        "center": {
+          "latitude": %{location.coordinates.lat#Float},
+          "longitude": %{location.coordinates.long#Float}},
+        "radius": 500.0"}}}|}])
+;;
+
+let call_api ~address ~key ~recommendation_type ~location =
+  Client.post ~body:(create_body ~recommendation_type ~location) ~headers:(create_recommendation_header key) (Uri.of_string (address)) >>= fun (_resp, body) ->
   let%bind body = Body.to_string body in
   (* print_endline body; *)
   return body
 ;;
-
-
-let find_nearby = 
-  let origin_address = config_recommendation_address ~street_address:(address) in
-  let%map place_id_origin_geocode = call_api ~configured_address:(Uri.of_string origin_address) in
-;; *)
+let find_nearby ~recommendation_type ~location = 
+  let%bind key = Lazy_deferred.force_exn api in
+  let address = "https://places.googleapis.com/v1/places:searchNearby" in
+  let%map response = call_api ~address ~key ~recommendation_type ~location in
+  response
+;;
